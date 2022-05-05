44a8bb2d692d8c9c26b6362d8858506359ceb2b4
Devyn Melendez <melend53@students.rowan.edu>
2022-05-05 01:30:10 -0400

    Delete Profbeats directory

--- Profbeats/messager/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Message
-
-# Create your forms here.
-
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ['recipient', 'subject', 'text', 'track_id']
